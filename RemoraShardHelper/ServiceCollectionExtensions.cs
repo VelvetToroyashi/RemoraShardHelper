0c9927bc2260d8d94bc0d614f890a68b15c29a01
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-09-14 11:43:25 -0400

    Hosting support.

---
+++ RemoraShardHelper/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Remora.Discord.Gateway.Extensions;
+using RemoraShardHelper.Services;
+
+namespace RemoraShardHelper;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDiscordSharding
+    (
+        this IServiceCollection services, 
+        Func<IServiceProvider, string> tokenFactory, 
+        Action<IHttpClientBuilder>? build = null
+    )
+    {
+        services.AddDiscordGateway(tokenFactory, build);
+
+        services.AddHostedService<ShardedDiscordService>();
+
+        return services;
+    }
+}
---
+++ RemoraShardHelper/Services/ShardedDiscordService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using Remora.Discord.Gateway.Results;
+using Remora.Discord.Hosting.Options;
+
+namespace RemoraShardHelper.Services;
+
+public class ShardedDiscordService : BackgroundService
+{
+    private readonly ShardedGatewayClient _client;
+    private readonly DiscordServiceOptions _options;
+    private readonly IHostApplicationLifetime _lifetime;
+    
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var runResult = await _client.RunAsync(stoppingToken);
+        
+        if (runResult.Error is GatewayError { IsCritical: true } && _options.TerminateApplicationOnCriticalGatewayErrors)
+        {
+            _lifetime.StopApplication();
+        }
+    }
+}
