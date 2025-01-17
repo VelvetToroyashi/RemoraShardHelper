aef7a16df2ef8c32edfdf426e0b91f7020ae8405
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2025-01-16 21:09:55 -0500

    refactor: Use UnsafeAccessors and bump to C#11/.NET 8/9

--- RemoraShardHelper/Services/ShardedGatewayClient.cs
+++ RemoraShardHelper/Services/ShardedGatewayClient.cs
@@ -1,14 +1,11 @@
 ﻿using System.Collections.Concurrent;
-using System.Reflection;
+using System.Runtime.CompilerServices;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Remora.Discord.API.Abstractions.Rest;
 using Remora.Discord.API.Gateway.Commands;
 using Remora.Discord.Gateway;
-using Remora.Discord.Gateway.Services;
-using Remora.Discord.Gateway.Transport;
-using Remora.Discord.Rest;
 using Remora.Results;
 
 namespace RemoraShardHelper.Services;
@@ -16,18 +13,17 @@ namespace RemoraShardHelper.Services;
 public class ShardedGatewayClient : IDisposable
 {
     private bool _isRunning;
-    
+
     private readonly IServiceProvider _services;
     private readonly IDiscordRestGatewayAPI _gatewayAPI;
-    private readonly ShardedGatewayClientOptions _gatewayOptions;
     private readonly ILogger<ShardedGatewayClient> _logger;
+    private readonly ShardedGatewayClientOptions _gatewayOptions;
 
     private readonly ConcurrentDictionary<int, DiscordGatewayClient> _gatewayClients = new();
-    
-    private static readonly FieldInfo _field = typeof(DiscordGatewayClient).GetField("_connectionStatus", BindingFlags.Instance | BindingFlags.NonPublic);
-    
-    private static readonly Func<DiscordGatewayClient, GatewayConnectionStatus> GetConnectionStatus = client => (GatewayConnectionStatus)_field.GetValue(client); 
-    
+
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_connectionStatus")]
+    private static extern GatewayConnectionStatus GetClientConnectionStatus(DiscordGatewayClient client);
+
     public IReadOnlyDictionary<int, DiscordGatewayClient> Shards => _gatewayClients;
 
     public ShardedGatewayClient
@@ -47,9 +43,9 @@ public class ShardedGatewayClient : IDisposable
     public async Task<Result> RunAsync(CancellationToken ct = default)
     {
         _isRunning = true;
-        
+
         var gatewayResult = await _gatewayAPI.GetGatewayBotAsync();
-        
+
         if (!gatewayResult.IsSuccess)
         {
             _logger.LogError("Failed to retrieve gateway endpoint");
@@ -60,28 +56,28 @@ public class ShardedGatewayClient : IDisposable
         {
             _gatewayOptions.ShardsCount ??= shards;
         }
-        
+
         if (_gatewayOptions.ShardIdentification is null && _gatewayOptions.ShardsCount is {} shardCount)
         {
             _gatewayOptions.ShardIdentification = new ShardIdentification(0, shardCount);
         }
-        
+
         var shardDelta = (_gatewayOptions.ShardIdentification?.ShardCount ?? _gatewayOptions.ShardsCount ?? 1) - ((_gatewayOptions.ShardIdentification?.ShardID ?? 0) + (_gatewayOptions.ShardsCount ?? 1));
 
         if (shardDelta > 0)
         {
             _logger.LogWarning("The specified shard count exceeds the set or recommended shard count " +
                                "of {ShardCount}, and only {Shards} will be started.",
-                _gatewayOptions.ShardIdentification.ShardCount, 
+                _gatewayOptions.ShardIdentification.ShardCount,
                 _gatewayOptions.ShardIdentification.ShardCount - shardDelta);
         }
-        
+
         var startupShards = (_gatewayOptions.ShardsCount ?? 1) - shardDelta;
 
         var maxStartup = gatewayResult.Entity.SessionStartLimit.IsDefined(out var sessionLimit) ? sessionLimit.MaxConcurrency : 1;
 
         var shardStride = _gatewayOptions.ShardIdentification?.ShardID ?? 0;
-        
+
         var clients = Enumerable
             .Range(shardStride, startupShards)
             .Select
@@ -91,7 +87,7 @@ public class ShardedGatewayClient : IDisposable
                     var client = ActivatorUtilities.CreateInstance<DiscordGatewayClient>(_services, CloneOptions(_gatewayOptions, s));
 
                     _gatewayClients[s] = client;
-                    
+
                     return client;
                 }
             )
@@ -101,9 +97,9 @@ public class ShardedGatewayClient : IDisposable
 
         // *Technically* Discord wants you to start n % max_concurrency, but this is *generally* fine, and I don't
         // think that's even strictly enforced on Discord's side either. It would be a pain to handle because we'd
-        // have to track for overflows, and only start shards that land in the first bucket, and then re-bucket and
+        // have to track for overflows, and only start shards that land in the first bucket, and then re-bucket, and
         // it's just a mess, frankly. Here's the docs about that: https://discord.dev/topics/gateway#sharding-max-concurrency
-        
+
         if (maxStartup is 1)
         {
             for (var shardIndex = 0; shardIndex < clients.Length; shardIndex++)
@@ -113,7 +109,7 @@ public class ShardedGatewayClient : IDisposable
 
                 tasks.Add(res);
 
-                while (GetConnectionStatus(client) is not GatewayConnectionStatus.Connected || res.IsCompleted)
+                while (GetClientConnectionStatus(client) is not GatewayConnectionStatus.Connected || res.IsCompleted)
                 {
                     await Task.Delay(100, ct);
                 }
@@ -122,7 +118,7 @@ public class ShardedGatewayClient : IDisposable
                 {
                     return res.Result;
                 }
-                
+
                 _logger.LogInformation("Started shard [{Shard}, {RealShardId}]", shardIndex, shardStride + shardIndex);
             }
         }
@@ -133,14 +129,14 @@ public class ShardedGatewayClient : IDisposable
             for (var burstIndex = 0; burstIndex < bursts.Length; burstIndex++)
             {
                 var burst = bursts[burstIndex];
-                
+
                 var now = DateTime.UtcNow;
                 const int startupDelay = 5000;
 
                 var startTasks = burst.Select(c => c.RunAsync(ct)).ToArray();
                 tasks.AddRange(startTasks);
 
-                while (burst.Any(c => GetConnectionStatus(c) is not GatewayConnectionStatus.Connected))
+                while (burst.Any(c => GetClientConnectionStatus(c) is not GatewayConnectionStatus.Connected))
                 {
                     if (startTasks.Any(t => t.IsCompleted && !t.Result.IsSuccess))
                     {
@@ -158,7 +154,7 @@ public class ShardedGatewayClient : IDisposable
                 {
                     _logger.LogInformation("Started shard [{Burst}, {Shard}, {RealShardId}]", burstIndex, startedShards + i, startedShards + shardStride + i);
                 }
-                
+
                 var requiredDelay = startupDelay - (DateTime.UtcNow - now).TotalMilliseconds;
 
                 if (requiredDelay > 0)
@@ -174,7 +170,7 @@ public class ShardedGatewayClient : IDisposable
     private IOptions<DiscordGatewayClientOptions> CloneOptions(DiscordGatewayClientOptions options, int shardID)
     {
         var ret = new DiscordGatewayClientOptions();
-        
+
         ret.ShardIdentification   = new ShardIdentification(shardID, options.ShardIdentification.ShardCount);
         ret.Intents               = options.Intents;
         ret.Presence              = options.Presence;
